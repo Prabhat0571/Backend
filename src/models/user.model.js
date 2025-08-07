13bd66b78666f52bde7e78f471e245dc668b5f69
Prabhat <prabhat22pk@gmail.com>
2025-08-07 14:45:49 +0530

    set up cloudinary

---
+++ src/models/user.model.js
@@ -0,0 +1,100 @@
+import mongoose from "mongoose";
+import jwt from 'jsonwebtoken'
+import bcrypt from 'bcrypt'
+import { use } from "react";
+const userSchema = mongoose.Schema(
+    {
+      username: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true, 
+            index: true //thoda searching optimise krdeta h but expensive operation
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowecase: true,
+            trim: true, 
+        },
+        fullName: {
+            type: String,
+            required: true,
+            trim: true, 
+            index: true
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        coverImage: {
+            type: String, // cloudinary url
+        },
+        watchHistory: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Video"
+            }
+        ],
+        password: {
+            type: String,
+            required: [true, 'Password is required'] //true field k sath ek custom error message de skte h
+            //hum hooks bhi use kar skte hai jese ki pre jo ek basically middleware hai
+        },
+        refreshToken: {
+            type: String
+        }
+
+    }
+,{timestamps: true})
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")) return next(); //agr password m kch change hi nahi hua h toh kch change mt karo bs next krado
+
+    this.password= bcrypt.hash(this.password,10) // change hua h toh kardo
+    //ye jo 10 itni baar round lgega for hashing
+    next() //flag aage badha do
+}) //jo bhi code execute karana h yaha likhdo only before a specific condition "save", "validation" etc
+
+//password kese compare kiya jaye kyuki jo database m store h wo toh encrypted hai and user jo de rha hai wo hai 12345 mtlb basic ek string hai
+//ye kaam bhi bcrypt kardeta hai /
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password) //ye compare karke true ya false return
+    //await isliye use kiya kyuki ye expensive operation hai and time lega toh isliye async await use kiya
+}
+
+userSchema.methods.generateAccessToken= function (){
+   return jwt.sign ({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullname: this.fullName //this.fullName comes fromd database
+    },
+          
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: process.env.ACCESS_TOKEN_EXPIRY //object k andr likhte hai
+
+    }
+)
+}
+
+userSchema.methods.generateRefreshToken= function (){
+   return jwt.sign ({
+        _id: this._id,
+    },
+          
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+        expiresIn: process.env.REFRESH_TOKEN_EXPIRY //object k andr likhte hai
+
+    }
+)
+}
+
+
+
+export const User= mongoose.model("User", userSchema)
